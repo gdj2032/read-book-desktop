ec91e9b4435cf941446ce1b1e06496457fe129d2
djgu <djgu@tmindtech.com>
2021-05-14 13:45:33 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import './App.css';
+
+interface IProps {
+}
+
+interface IState {
+}
+
+class App extends Component<IProps, IState> {
+
+  state: IState = {}
+
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App;
